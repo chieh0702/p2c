bc40710f96b2c2336dc11433215d9f2132fe3a8b
chieh0702 <yj6007ress@gmail.com>
2023-11-11 21:03:56 +0800

    gui modify

--- src/gui/script.js
+++ src/gui/script.js
@@ -44,6 +44,10 @@ function genListBox(name, context) {
         listbox.appendChild(new_viewbox());
     });
     titleview.appendChild(plusicon);
+    const trashicon = new_trashicon();
+    trashicon.addEventListener("click", () => {
+        listgp.remove();
+    });
     titleview.appendChild(trashicon);
     listgp.appendChild(titleview);
     const listbox = document.createElement("div");
@@ -73,20 +77,23 @@ function new_viewbox(context) {
         textinput.focus();
     });
     viewbox.appendChild(option);
+    const trashicon = new_trashicon();
+    trashicon.addEventListener("click", () => {
+        viewbox.remove();
+    });
+    viewbox.appendChild(trashicon);
+    return viewbox;
+}
+
+function new_trashicon() {
     const trashicon = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
     trashicon.setAttribute("height", "1em");
     trashicon.setAttribute("viewBox", "0 0 448 512");
     const path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
     path.setAttribute("d", "M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z");
     trashicon.appendChild(path);
-    trashicon.addEventListener("click", () => {
-        viewbox.remove();
-    });
-    viewbox.appendChild(trashicon);
-    return viewbox
+    return trashicon;
 }
-
-
 
 
 genListBox("Command", ["--gui", "--website"]);
